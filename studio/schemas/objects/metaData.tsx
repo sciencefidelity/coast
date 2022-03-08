41728d6373933fa8f83be39ad18126eb7553d024
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-08 23:35:40 +0000

    feat: add a character counter to validated strings

--- studio/schemas/objects/metaData.tsx
+++ studio/schemas/objects/metaData.tsx
@@ -1,3 +1,5 @@
+import StringWithLimits from '../components/StringWithLimits'
+
 export default {
   name: 'metaData',
   title: 'Meta data',
@@ -8,14 +10,17 @@ export default {
       name: 'title',
       title: 'Meta Title',
       type: 'string',
-      description: 'Recommended: 70 characters.' // You’ve used 0
+      inputComponent: StringWithLimits,
+      // description: 'Recommended: 70 characters.', // You’ve used 0
+      validation: Rule => Rule.max(70)
     },
     {
       name: 'description',
       title: 'Meta Description',
       type: 'text',
       rows: 3,
-      description: 'Recommended: 156 characters.' // You’ve used 0
+      // description: 'Recommended: 156 characters.' // You’ve used 0
+      validation: Rule => Rule.max(156)
     },
     {
       name: 'canonicalURL',
