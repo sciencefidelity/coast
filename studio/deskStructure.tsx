aa3586a3373f02da000d6c5adcbe6b7b0d281a12
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-12 21:56:40 +0000

    refactor: change settings, design and navigation to singletons

---
+++ studio/deskStructure.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import S from '@sanity/desk-tool/structure-builder'
+
+export default () =>
+S.list()
+  .title('Content')
+  .items(
+    [
+      ...S.documentTypeListItems().filter(item => !['settings', 'design', 'navigation'].includes(item.getId())),
+      S.divider(),
+      S.listItem()
+        .title('Settings')
+        .child(
+          S.document()
+            .schemaType('settings')
+            .documentId('settings')
+        )
+        .icon(() => (
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 24 24"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g>
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path d="M8.686 4l2.607-2.607a1 1 0 0 1 1.414 0L15.314 4H19a1 1 0 0 1 1 1v3.686l2.607 2.607a1 1 0 0 1 0 1.414L20 15.314V19a1 1 0 0 1-1 1h-3.686l-2.607 2.607a1 1 0 0 1-1.414 0L8.686 20H5a1 1 0 0 1-1-1v-3.686l-2.607-2.607a1 1 0 0 1 0-1.414L4 8.686V5a1 1 0 0 1 1-1h3.686zM6 6v3.515L3.515 12 6 14.485V18h3.515L12 20.485 14.485 18H18v-3.515L20.485 12 18 9.515V6h-3.515L12 3.515 9.515 6H6zm6 10a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"></path>
+            </g>
+          </svg>
+        )),
+      S.listItem()
+        .title('Design')
+        .child(
+          S.document()
+            .schemaType('design')
+            .documentId('design')
+        )
+        .icon(() => (
+          <svg
+            stroke="currentColor"
+            fill="none"
+            strokeWidth="0"
+            viewBox="0 0 24 24"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+            ></path>
+          </svg>
+        )),
+      S.listItem()
+        .title('Navigation')
+        .child(
+          S.document()
+            .schemaType('navigation')
+            .documentId('navigation')
+        )
+        .icon(() => (
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 24 24"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g>
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path d="M4.965 5.096l3.546 12.41 3.04-6.08 5.637-2.255L4.965 5.096zM2.899 2.3l18.806 6.268a.5.5 0 0 1 .028.939L13 13l-4.425 8.85a.5.5 0 0 1-.928-.086L2.26 2.911A.5.5 0 0 1 2.9 2.3z"></path>
+            </g>
+          </svg>
+        ))
+    ]
+  )
