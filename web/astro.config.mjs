144ce6b20f044cd9ce101bb3a17f016e79c9ecd0
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-29 21:19:46 -0200

    chore: upgrade and migrate Astro

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,26 +1,27 @@
-import { resolve } from "path"
+import { defineConfig } from "astro/config"
+// import { resolve } from "path"
+import react from "@astrojs/react"
+import svelte from "@astrojs/svelte"
 
-export default {
-  projectRoot: '.',
-  // https://docs.astro.build/en/reference/configuration-reference/
+export default defineConfig({
   buildOptions: {
     site: "https://in-our-hands.com",
     sitemap: true
   },
   devOptions: {
-    hostname: "0.0.0.0",
+    host: true,
   },
-  renderers: ["@astrojs/renderer-react", "@astrojs/renderer-svelte"],
-  vite: {
-    resolve: {
-      alias: {
-        "@": resolve("./src"),
-        "components": resolve("./src/components"),
-        "layouts": resolve("./src/layouts"),
-        "lib": resolve("./src/lib"),
-        "pages": resolve("./src/pages"),
-        "styles": resolve("./src/styles")
-      }
-    }
-  }
-}
+  integrations: [react(), svelte()]
+  // vite: {
+  //   resolve: {
+  //     alias: {
+  //       "@": resolve("./src"),
+  //       "components": resolve("./src/components"),
+  //       "layouts": resolve("./src/layouts"),
+  //       "lib": resolve("./src/lib"),
+  //       "pages": resolve("./src/pages"),
+  //       "styles": resolve("./src/styles")
+  //     }
+  //   }
+  // }
+})
