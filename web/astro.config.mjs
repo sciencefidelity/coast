00b973731571264a75e19f9364cb4a3270e67639
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-31 16:30:03 +0100

    refactor: set up aliases not Astro LPS supports them

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config"
-import { resolve } from "path"
+// import { resolve } from "path"
 import react from "@astrojs/react"
 import svelte from "@astrojs/svelte"
 
@@ -12,16 +12,16 @@ export default defineConfig({
     host: true,
   },
   integrations: [react(), svelte()],
-  vite: {
-    resolve: {
-      alias: {
-        "@": resolve("./src"),
-        "components": resolve("./src/components"),
-        "layouts": resolve("./src/layouts"),
-        "lib": resolve("./src/lib"),
-        "pages": resolve("./src/pages"),
-        "styles": resolve("./src/styles")
-      }
-    }
-  }
+  // vite: {
+  //   resolve: {
+  //     alias: {
+  //       "@": resolve("./src"),
+  //       "components": resolve("./src/components"),
+  //       "layouts": resolve("./src/layouts"),
+  //       "lib": resolve("./src/lib"),
+  //       "pages": resolve("./src/pages"),
+  //       "styles": resolve("./src/styles")
+  //     }
+  //   }
+  // }
 })
