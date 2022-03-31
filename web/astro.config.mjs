f55d85e373eb7684b7427a0b37a8295681528bbe
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-30 23:42:01 -0200

    build: add aliases to vite config

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config"
-// import { resolve } from "path"
+import { resolve } from "path"
 import react from "@astrojs/react"
 import svelte from "@astrojs/svelte"
 
@@ -11,17 +11,17 @@ export default defineConfig({
   devOptions: {
     host: true,
   },
-  integrations: [react(), svelte()]
-  // vite: {
-  //   resolve: {
-  //     alias: {
-  //       "@": resolve("./src"),
-  //       "components": resolve("./src/components"),
-  //       "layouts": resolve("./src/layouts"),
-  //       "lib": resolve("./src/lib"),
-  //       "pages": resolve("./src/pages"),
-  //       "styles": resolve("./src/styles")
-  //     }
-  //   }
-  // }
+  integrations: [react(), svelte()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": resolve("./src"),
+        "components": resolve("./src/components"),
+        "layouts": resolve("./src/layouts"),
+        "lib": resolve("./src/lib"),
+        "pages": resolve("./src/pages"),
+        "styles": resolve("./src/styles")
+      }
+    }
+  }
 })
