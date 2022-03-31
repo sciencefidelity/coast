d3efc7eadb487fe2ce704bc8e2c61ab197fc509d
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-31 16:44:18 +0100

    chore: remove unused node modules

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,27 +1,13 @@
 import { defineConfig } from "astro/config"
-// import { resolve } from "path"
-import react from "@astrojs/react"
-import svelte from "@astrojs/svelte"
+import tailwind from "@astrojs/tailwind"
 
+// https://astro.build/config
 export default defineConfig({
   buildOptions: {
-    site: "https://in-our-hands.com",
     sitemap: true
   },
   devOptions: {
-    host: true,
+    host: true
   },
-  integrations: [react(), svelte()],
-  // vite: {
-  //   resolve: {
-  //     alias: {
-  //       "@": resolve("./src"),
-  //       "components": resolve("./src/components"),
-  //       "layouts": resolve("./src/layouts"),
-  //       "lib": resolve("./src/lib"),
-  //       "pages": resolve("./src/pages"),
-  //       "styles": resolve("./src/styles")
-  //     }
-  //   }
-  // }
+  integrations: [tailwind()]
 })
