15e0446c9b1db63631279317fde4723df956c318
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-04 23:56:56 +0100

    refactor: update author type

--- studio/schemas/documents/author.ts
+++ studio/schemas/documents/author.ts
@@ -1,18 +1,19 @@
 // import { RiTeamLine } from 'react-icons/ri'
+import {defineType, defineField} from 'sanity'
 
-export default {
+export default defineType({
   name: 'author',
   title: 'Author',
   type: 'document',
   // icon: RiTeamLine,
   fields: [
-    {
+    defineField({
       name: 'name',
       title: 'Full name',
       type: 'string',
       description: 'Use your real name so people can recognize you',
-    },
-    {
+    }),
+    defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
@@ -20,50 +21,50 @@ export default {
         source: 'name',
         maxLength: 96,
       },
-    },
-    {
+    }),
+    defineField({
       name: 'email',
       title: 'Email',
       type: 'email',
-    },
-    {
+    }),
+    defineField({
       name: 'location',
       title: 'Location',
       type: 'string',
       description: 'Where in the world do you live?',
-    },
-    {
+    }),
+    defineField({
       name: 'website',
       title: 'Website',
       type: 'url',
       description: 'Have a website or blog other than this one? Link it!',
-    },
-    {
+    }),
+    defineField({
       name: 'facebook',
       title: 'Facebook profile',
       type: 'url',
       description: 'URL of your personal Facebook Profile',
-    },
-    {
+    }),
+    defineField({
       name: 'twitter',
       title: 'Twitter profile',
       type: 'url',
       description: 'URL of your personal Twitter Profile',
-    },
-    {
+    }),
+    defineField({
       name: 'body',
       title: 'Bio',
       type: 'text',
       description: 'Recommended: 200 characters.',
-    },
-    {
+    }),
+    defineField({
       name: 'image',
       title: 'Avatar',
       type: 'image',
       options: {
         hotspot: true,
       },
-    },
+    }),
   ],
   preview: {
     select: {
@@ -71,4 +72,4 @@ export default {
       media: 'image',
     },
   },
-}
+})
